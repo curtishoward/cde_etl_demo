a4f9ffbcf1b69d219b137de1b75adcce46eb4454
Curtis Howard <curtis@cloudera.com>
2021-12-08 21:40:49 -0500

    initial commit

---
+++ dag.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from cloudera.cdp.airflow.operators.cde_operator import CDEJobRunOperator
+
+####### CHANGE THIS ##########
+PREFIX = 'your_username'
+##############################
+
+
+INGEST_JOB = PREFIX + '-ingest'
+ENRICH_JOB = PREFIX + '-enrich'
+DAG_NAME   = PREFIX + '_demo_dag'
+
+default_args = {
+    'owner': 'cdeuser1',
+    'retry_delay': timedelta(seconds=5),
+    'start_date': datetime(2021,1,1,1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+dag = DAG(
+    DAG_NAME,
+    default_args=default_args,
+    catchup=False,
+    is_paused_upon_creation=False
+)
+start = DummyOperator(task_id='start', dag=dag)
+end = DummyOperator(task_id='end', dag=dag)
+
+ingest_task = CDEJobRunOperator(
+    task_id='ingest',
+    dag=dag,
+    job_name=INGEST_JOB
+)
+
+enrich_task = CDEJobRunOperator(
+    task_id='enrich',
+    dag=dag,
+    job_name=ENRICH_JOB
+)
+
+start >> ingest_task >> enrich_task >> end
